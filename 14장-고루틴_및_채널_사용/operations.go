e9ab84c6bddac8aaee430849bcf77df08211f3a7
steamedEggMaster <iron.ch5014@gmail.com>
2025-06-14 14:25:11 +0900

    feat: 버퍼 채널 시작전 저장

--- 14장-고루틴_및_채널_사용/operations.go
+++ 14장-고루틴_및_채널_사용/operations.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func CalcStoreTotal(data ProductData) {
@@ -10,8 +11,13 @@ func CalcStoreTotal(data ProductData) {
 	for category, group := range data {
 		go group.TotalPrice(category, channel)
 	}
+	time.Sleep(time.Second * 5)
 	for i := 0; i < len(data); i++ {
-		storeTotal += <-channel
+		fmt.Println("-- channel read pending")
+		value := <-channel
+		fmt.Println("-- channel read complete", value)
+		storeTotal += value
+		time.Sleep(time.Second)
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
 }
@@ -19,10 +25,11 @@ func CalcStoreTotal(data ProductData) {
 func (group ProductGroup) TotalPrice(category string, resultChannel chan float64) {
 	var total float64
 	for _, p := range group {
-		fmt.Println(category, "product:", p.Name)
+		// fmt.Println(category, "product:", p.Name)
 		total += p.Price
 		// time.Sleep(time.Millisecond * 100)
 	}
-	fmt.Println(category, "subtotal:", ToCurrency(total))
+	fmt.Println(category, "channel sending", ToCurrency(total))
 	resultChannel <- total
+	fmt.Println(category, "channel send complete")
 }
