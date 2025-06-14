d3ac4e5b2d2040d122cb3141414e22530471a56f
steamedEggMaster <iron.ch5014@gmail.com>
2025-06-14 14:15:49 +0900

    feat: 채널 조정 시작전 저장

--- 14장-고루틴_및_채널_사용/operations.go
+++ 14장-고루틴_및_채널_사용/operations.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
+	var channel chan float64 = make(chan float64)
 	for category, group := range data {
-		go group.TotalPrice(category)
+		go group.TotalPrice(category, channel)
+	}
+	for i := 0; i < len(data); i++ {
+		storeTotal += <-channel
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
 }
 
-func (group ProductGroup) TotalPrice(category string) (total float64) {
+func (group ProductGroup) TotalPrice(category string, resultChannel chan float64) {
+	var total float64
 	for _, p := range group {
 		fmt.Println(category, "product:", p.Name)
 		total += p.Price
-		time.Sleep(time.Millisecond * 100)
+		// time.Sleep(time.Millisecond * 100)
 	}
 	fmt.Println(category, "subtotal:", ToCurrency(total))
-	return
+	resultChannel <- total
 }
