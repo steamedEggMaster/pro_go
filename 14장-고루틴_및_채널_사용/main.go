9977de54908d57a1aa2b303d46d0bd6a3bcbaeed
steamedEggMaster <iron.ch5014@gmail.com>
2025-06-14 14:49:40 +0900

    feat: 채널 값 열거  시작전 저장

--- 14장-고루틴_및_채널_사용/main.go
+++ 14장-고루틴_및_채널_사용/main.go
@@ -1,13 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("main function started")
-	CalcStoreTotal(Products)
-	// 리스트 14-8
-	// time.Sleep(time.Second * 5)
-	fmt.Println("main function complete")
+	// fmt.Println("main function started")
+	// CalcStoreTotal(Products)
+	// // 리스트 14-8
+	// // time.Sleep(time.Second * 5)
+	// fmt.Println("main function complete")
+
+	// ------- 미정 개수 값 전송 및 수신 ---------
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrders(dispatchChannel)
+	for {
+		if details, open := <-dispatchChannel; open {
+			fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+		} else {
+			fmt.Println("Channel has been closed")
+			break
+		}
+	}
 }
