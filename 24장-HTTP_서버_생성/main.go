c4eb621712f58933b211a7489da83f2f4e723ad7
steamedEggMaster <iron.ch5014@gmail.com>
2025-06-17 08:55:47 +0900

    24장까지 완료

---
+++ 24장-HTTP_서버_생성/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+type StringHandler struct {
+	message string
+}
+
+func (sh StringHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	io.WriteString(writer, sh.message)
+}
+
+func main() {
+	err := http.ListenAndServe(":5000", StringHandler{message: "Hello, World"})
+	if err != nil {
+		println("error : %v", err.Error())
+	}
+}
